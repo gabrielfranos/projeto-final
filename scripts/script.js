7bf8b5e407f6a87bb3e391d974eabeebdc9bc732
gabrielfranos <gabrielfranos@gmail.com>
2023-12-08 11:46:47 -0300

    update_save-cart

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,28 @@
 var cart = [];
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 const btnCart = document.querySelectorAll('.btnCart');
+const fullCart = document.getElementById('fullCart');
+const emptyCart = document.getElementById('emptyCart');
+const btnSalvar = document.getElementById('btnSalvar');
+
+btnSalvar.addEventListener('click', function(){
+    sessionStorage.setItem('valorFinal',JSON.stringify(cart));
+})
+
+function atualizaCart(){
+if (cart.length == 0){
+    fullCart.style.display = 'none'
+    emptyCart.style.display = 'flex'
+} else {
+    fullCart.style.display = 'flex'
+    fullCart.classList.add('flex-column','gap-1')
+    emptyCart.style.display = 'none'
+}
+}
+
 
 function adicionarCart(nomeItem, precoItem) {
     let item = {
@@ -9,6 +31,7 @@ function adicionarCart(nomeItem, precoItem) {
         quantidade: 1
     };
     cart.push(item);
+    atualizaCart();
 }
 
 btnCart.forEach(function(botao) {
@@ -25,34 +48,48 @@ btnCart.forEach(function(botao) {
             adicionarCart(nomeItem, precoItem);
         }
         atualizaTotal();
+        atualizaCart();
     });
 });
 
 function removeItem(index) {
     cart.splice(index, 1);
     atualizaTotal();
+    atualizaCart();
 }
 
 function atualizaTotal() {
     let totalCar = totalCompra();
     document.getElementById('totalCarrinho').innerHTML = `Total: R$ ${totalCar}`;
-    const carrinhoItems = document.getElementById('carrinhoItems');
-    carrinhoItems.innerHTML = '';
+    const carrinho = document.getElementById('carrinho');
+    carrinho.innerHTML = '';
 
     cart.forEach((item, index) => {
         const itemCompra = document.createElement('div');
-        itemCompra.classList.add('itemCarrinho');
+        itemCompra.classList.add('itemCompra')
+        const itemInfo = document.createElement('div');
+        itemInfo.classList.add('itemInfo');
 
-        const itemInfo = document.createElement('p');
-        itemInfo.innerHTML = `${item.nome} - R$ ${item.preco} - Quantidade: ${item.quantidade}`;
-        itemCompra.appendChild(itemInfo);
+        const itemNomePreco = document.createElement('p');
+        itemNomePreco.innerHTML = `<b>${item.nome} </b>   <small> R$ ${item.preco} </small>`;
+        itemInfo.appendChild(itemNomePreco);
+
+        const itemQtd = document.createElement('p');
+        itemQtd.innerHTML = `<small> Quantidade: ${item.quantidade} </small>`
+        itemInfo.appendChild(itemQtd);
 
         const removeButton = document.createElement('button');
-        removeButton.innerHTML = 'Remove';
+        removeButton.classList.add('btn','icon-link' )
+        removeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" class="bi bi-x-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+      </svg> Remover `;
         removeButton.addEventListener('click', () => removeItem(index));
+        itemCompra.appendChild(itemInfo);
         itemCompra.appendChild(removeButton);
 
-        carrinhoItems.appendChild(itemCompra);
+        carrinho.appendChild(itemCompra);
+        
     });
 }
 
