4b0279bc09e739edacd51ee728cc6d6140c56696
gabrielfranos <gabrielfranos@gmail.com>
2023-12-08 09:47:17 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,66 @@
-var cart =[];
+var cart = [];
 
 const btnCart = document.querySelectorAll('.btnCart');
 
-function adicionarCart(nomeItem,precoItem){
+function adicionarCart(nomeItem, precoItem) {
     let item = {
         nome: nomeItem,
-        preco: precoItem
+        preco: precoItem,
+        quantidade: 1
     };
     cart.push(item);
 }
 
-
-btnCart.forEach(function(botao){
-    botao.addEventListener('click', function(){
+btnCart.forEach(function(botao) {
+    botao.addEventListener('click', function() {
         let divCard = botao.closest('.card-body');
         let nomeItem = divCard.querySelector('.card-title').textContent;
         let preco = divCard.querySelector('.card-price').textContent;
-        let precoItem = Number(preco.replace('R$','').replace(',','.'));
+        let precoItem = Number(preco.replace('R$', '').replace(',', '.'));
 
+        let itemIndex = cart.findIndex(item => item.nome === nomeItem);
+        if (itemIndex !== -1) {
+            cart[itemIndex].quantidade++;
+        } else {
+            adicionarCart(nomeItem, precoItem);
+        }
+        atualizaTotal();
+    });
+});
 
-        adicionarCart(nomeItem, precoItem);
-        atualizaTotal()
-    })
-})
-
-function atualizaTotal(){
-    let totalCar = totalCompra();
-    document.getElementById('totalCarrinho').innerHTML = `Total: R$ ${totalCar}`
-    const carrinhoItems = document.getElementById('carrinhoItems')
-    const itemCompra = document.createElement('p');
-    itemCompra.classList.add('itemCarrinho');
-
-    nomeCart = cart.map((x) => x.nome);
-    precoCart = cart.map((x) => x.preco);
-
-    itemCompra.innerHTML = `${nomeCart.pop()} - ${precoCart.pop()}`
-    carrinhoItems.appendChild(itemCompra)
-    
+function removeItem(index) {
+    cart.splice(index, 1);
+    atualizaTotal();
 }
 
+function atualizaTotal() {
+    let totalCar = totalCompra();
+    document.getElementById('totalCarrinho').innerHTML = `Total: R$ ${totalCar}`;
+    const carrinhoItems = document.getElementById('carrinhoItems');
+    carrinhoItems.innerHTML = '';
 
-function totalCompra(){
+    cart.forEach((item, index) => {
+        const itemCompra = document.createElement('div');
+        itemCompra.classList.add('itemCarrinho');
+
+        const itemInfo = document.createElement('p');
+        itemInfo.innerHTML = `${item.nome} - R$ ${item.preco} - Quantidade: ${item.quantidade}`;
+        itemCompra.appendChild(itemInfo);
+
+        const removeButton = document.createElement('button');
+        removeButton.innerHTML = 'Remove';
+        removeButton.addEventListener('click', () => removeItem(index));
+        itemCompra.appendChild(removeButton);
+
+        carrinhoItems.appendChild(itemCompra);
+    });
+}
+
+function totalCompra() {
     var total = 0;
 
-    for (let i=0; i<cart.length;i++){
-        total += cart[i].preco
+    for (let i = 0; i < cart.length; i++) {
+        total += cart[i].preco * cart[i].quantidade;
     }
 
     return total.toFixed(2);
